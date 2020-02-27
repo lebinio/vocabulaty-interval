0b9194d7a4f1b72a19d0ff6b1f707ddbf425c38a
Binh Le <lebinh.xz@gmail.com>
2020-02-28 02:18:49 +0700

    add header block

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import { Container, Button, Grid } from '@material-ui/core';
+import { Container, Grid, Typography, Divider } from '@material-ui/core';
 
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
-      <Container>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          style={{ height: '100%' }}
-        >
+      <Container className={styles.mainContainer}>
+        <Grid container direction="column" justify="center" alignItems="center">
           <Grid item>
-            <Button variant="outlined" color="primary">STARTA</Button>
+            <Typography variant="h6" color="primary">abide by (v)</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" color="secondary">/ə'baid/</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1">tôn trọng, tuân theo, giữ (lời)</Typography>
+          </Grid>
+          <Grid item container direction="row" justify="center" alignItems="center">
+            <Grid className={styles.example}>
+              <Typography variant="body2" display="block">
+                The two parties agreed to abide by the judge's decision
+              </Typography>
+            </Grid>
+            <Divider orientation="vertical" flexItem />
+            <Grid className={styles.example}>
+              <Typography variant="body2" display="block">
+                Hai bên đã đồng ý tuân theo quyết định của tòa án.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
