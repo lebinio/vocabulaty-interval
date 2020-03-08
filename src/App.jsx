e96e42eeaec86680f07c74e40a9c2a20794bddd2
Binh Le <lebinh.xz@gmail.com>
2020-03-09 00:18:11 +0700

    add react-visualized and words data

--- src/App.jsx
+++ src/App.jsx
@@ -4,91 +4,84 @@ import {
   Grid,
   Typography,
   Divider,
+  ListItem,
+  ListItemText,
 } from '@material-ui/core';
-import Electron from 'electron';
+import { List } from 'react-virtualized';
 
+import DATA from './data/data';
 import styles from './App.module.css';
 
-const { BrowserWindow, screen, getCurrentWindow } = Electron.remote;
+function rowRenderer({
+  key, //
+  index,
+  style,
+}) {
+  return (
+    <ListItem button key={key} style={style}>
+      <ListItemText>{DATA[index].original}</ListItemText>
+    </ListItem>
+  );
+}
 
 function App() {
-  useEffect(() => {
-    const mainWindow = getCurrentWindow();
-
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-
-    let win = new BrowserWindow({
-      parent: mainWindow,
-      x: width - 320,
-      y: height - 200,
-      width: 320,
-      height: 200,
-      resizable: false,
-      movable: false,
-      alwaysOnTop: true,
-      frame: false,
-      transparent: true,
-    });
-    win.on('closed', () => {
-      win = null;
-    });
-
-    const html = `<html lang="en">
-      <head>
-        <title>Popover</title>
-        <style>
-          .popover-container {
-            height: 100%;
-            width: 100%;
-          }
-        </style>
-      </head>
-      <body>
-        <div class="popover-container">
-          <h1>Welcome, this is the Index page.</h1>
-          <h3>Hello there</h3>
-        </div>
-      </body>
-      </html>`;
-
-    function closePopover() {
-      win.close();
-    }
-
-    win.loadURL(`data:text/html;charset=utf-8,${encodeURI(html)}`);
-    setTimeout(closePopover, 5000);
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <>
       <Container className={styles.mainContainer}>
         <Grid container direction="column" justify="center" alignItems="center">
-          <Grid item>
-            <Typography variant="h6" color="primary">
-              abide by (v)
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" color="secondary">
-              /ə'baid/
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body1">tôn trọng, tuân theo, giữ (lời)</Typography>
-          </Grid>
-          <Grid item container direction="row" justify="center" alignItems="center">
-            <Grid className={styles.example}>
-              <Typography variant="body2" display="block">
-                The two parties agreed to abide by the judge's decision
+          <Grid container item direction="column" justify="center" alignItems="center">
+            <Grid item>
+              <Typography variant="h6" color="primary">
+                abide by (v)
               </Typography>
             </Grid>
-            <Divider orientation="vertical" flexItem />
-            <Grid className={styles.example}>
-              <Typography variant="body2" display="block">
-                Hai bên đã đồng ý tuân theo quyết định của tòa án.
+            <Grid item>
+              <Typography variant="subtitle1" color="secondary">
+                /ə'baid/
               </Typography>
             </Grid>
+            <Grid item>
+              <Typography variant="body1">tôn trọng, tuân theo, giữ (lời)</Typography>
+            </Grid>
+            <Grid item container direction="row" justify="center" alignItems="center">
+              <Grid className={styles.example}>
+                <Typography variant="body2" display="block">
+                  The two parties agreed to abide by the judge's decision
+                </Typography>
+              </Grid>
+              <Divider orientation="vertical" flexItem />
+              <Grid className={styles.example}>
+                <Typography variant="body2" display="block">
+                  Hai bên đã đồng ý tuân theo quyết định của tòa án.
+                </Typography>
+              </Grid>
+            </Grid>
           </Grid>
+        </Grid>
+        <Grid
+          container
+          item
+          direction="row"
+          justify="center"
+          alignItems="center"
+          style={{ marginTop: '20px' }}
+        >
+          <List
+            width={300}
+            height={300}
+            rowCount={DATA.length}
+            rowHeight={40}
+            rowRenderer={rowRenderer}
+          />
+          <List
+            width={300}
+            height={300}
+            rowCount={DATA.length}
+            rowHeight={40}
+            rowRenderer={rowRenderer}
+          />
         </Grid>
       </Container>
     </>
